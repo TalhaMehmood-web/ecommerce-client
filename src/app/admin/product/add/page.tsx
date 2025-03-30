b36789cb89dd9710be78701b45e4e2825064d923
talha7272 <talha.mehmood@pwr.team>
2025-03-30 17:10:38 +0500

    deleted extra pages

--- src/app/admin/product/add/page.tsx
+++ src/app/admin/product/add/page.tsx
@@ -1,22 +1,23 @@
 import ProductAdd from "@/components/app/(admin)/new-product";
-import AddProductView from "@/components/app/(admin)/product/add";
 import PageWrapper from "@/components/shared/page-wrapper";
 import { PageBreadCrumb } from "@/components/shared/page-wrapper/breadcrumb";
 import PageTitle from "@/components/shared/page-wrapper/page-title";
-import React from "react";
+import React, { Suspense } from "react";
 
 const AddProductPage = () => {
   return (
-    <PageWrapper className="flex flex-col gap-1.5">
-      <PageBreadCrumb
-        items={[
-          { title: "Dashboard", href: "/admin/dashboard" },
-          { title: "Add Product" },
-        ]}
-      />
-      <PageTitle title="Add Product" />
-      <ProductAdd />;
-    </PageWrapper>
+    <Suspense fallback={<p>Loading</p>}>
+      <PageWrapper className="flex flex-col gap-1.5">
+        <PageBreadCrumb
+          items={[
+            { title: "Dashboard", href: "/admin/dashboard" },
+            { title: "Add Product" },
+          ]}
+        />
+        <PageTitle title="Add Product" />
+        <ProductAdd />;
+      </PageWrapper>
+    </Suspense>
   );
 };
 
